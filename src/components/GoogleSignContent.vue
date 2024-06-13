6ada49953eaf04ab266e7a5fc2f70ca362db47bd
Lovethrech <rachealdolapo45@gmail.com>
2024-06-13 22:25:29 +0100

    working on google auth first reference

---
+++ src/components/GoogleSignContent.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { ref } from "vue";
+import OnBoardingText from "@/components/OnBoardingText.vue";
+import OnBoardingBtn from "@/components/OnBoardingBtn.vue";
+
+const gMail= ref("")
+const displayBtn = ref(false);
+const submitMail = () => {
+    if ((gMail.value === "") || (gMail.value === null)) {
+        displayBtn.value = false;
+    }
+    else {
+        displayBtn.value = true;
+    }
+}
+</script>
+
+<style scoped>
+.on-boarding-content-main-ctn {
+    background-color: rgb(255, 255, 255);
+    width: 60%;
+    height: 95vh;
+    display: flex;
+    flex-direction:column;
+    place-items: end;
+    justify-content:end;
+}
+.on-boarding-content-ctn {
+    color:black;
+    width: 90%;
+    height: 70%;
+    border-top-left-radius: 450px;
+    padding: 0 6vw;
+}
+.home-btn-ctn{
+    font-size: 32px;
+}
+.link{
+    color:black;
+    padding:0;
+    margin:0;
+}
+.title, .desc, .email-ctn{
+    margin: 0 10px;
+}
+.title{
+    margin-top:-10px;
+    font-size:24px;
+    font-weight:600;
+}
+.desc{
+    color:#aba0a0;
+}
+.email-ctn input::placeholder{
+    color:#aba0a0;
+}
+.email-ctn, .email-ctn input{
+    width:100%;
+    font-size:20px;
+    border:0; 
+}
+.email-ctn{
+    border-bottom:2px solid black;
+}
+.submit-link{
+    background: #280d0d;
+    color:white;
+    padding:10px 0;
+    font-size: 22px;
+    font-weight:600;
+    letter-spacing: 0.5vw;
+    width: 100%;
+    text-align:center;
+}
+</style>
+
+<template>
+    <div class="on-boarding-content-main-ctn">
+        <div class="on-boarding-content-ctn">
+            
+            <div class="home-btn-ctn">
+                <RouterLink to="/" class="link">
+                    <i class='bx bx-chevron-left'></i>
+                </RouterLink> 
+            </div>
+            
+            <h3 class="title">Let's get started with your email:</h3>
+            <p class="desc">*Indicate required field</p>
+            <br><br><br>
+            <div class="email-ctn" @click="submitMail">
+                <input v-model="gMail" type="email" name="email" placeholder="*Email"/>
+            </div>
+        </div>
+        <RouterLink to="/" class="submit-link" v-show="displayBtn">Continue</RouterLink>
+    </div>
+</template>
---
+++ src/views/GoogleSignView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import HomeFoodSection from "@/components/HomeFoodSection.vue";
+import GoogleSignContent from "@/components/GoogleSignContent.vue";
+</script>
+
+<style scoped>
+.google-sign-content-main-ctn {
+    background-color: #162114;
+    display: flex;
+
+    width: 100%;
+    height: 100vh;
+    padding: 0;
+}
+
+.google-sign-content {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    border-radius: 700px 0 0 0;
+}
+</style>
+
+<template>
+    <div class="google-sign-content-main-ctn">
+        <HomeFoodSection />
+        <GoogleSignContent class="google-sign-content" />
+    </div>
+</template>
